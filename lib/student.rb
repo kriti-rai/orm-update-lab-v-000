125af61374cdb72c6719ffad89ca4afc9e23e1b3
Kriti Rai <kriti.rai@gmail.com>
2018-04-25 20:13:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,14 +60,7 @@ class Student
   end
 
   def self.new_from_db
-    sql = <<-SQL
-      SELECT *
-      FROM students
-    SQL
-
-    DB[:conn].execute(sql).map do |row|
-      self.create(row[1],row[2])
-    end.first
+    self.create(row[1],row[2])
   end
 
   def self.find_by_name(name)
