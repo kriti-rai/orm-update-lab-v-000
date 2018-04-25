b1e80552d0816bea5b399b91dee9143c428b2e2c
Kriti Rai <kriti.rai@gmail.com>
2018-04-25 20:01:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,9 +56,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.create(row[1],row[2])
-    student
-    binding.pry
+    self.create(row[1],row[2])
   end
 
   def self.find_by_name(name)
