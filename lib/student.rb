57a1259f769fe0399f3ed141354517bfb5c25e66
Kriti Rai <kriti.rai@gmail.com>
2018-04-25 19:32:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ class Student
       self.update
     else
       sql = <<-SQL
-        INSERT INTO students (name, grade)
+        INSERT INTO students (name,grade)
         VALUES (?,?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
@@ -60,7 +60,7 @@ class Student
   end
 
   def self.find_by_name(name)
-    row = DB[:conn].execute("SELECT * FROM students WHERE name = ? LIMIT 1", name)[0]
+    row = DB[:conn].execute("SELECT * FROM students WHERE name = ?", name)[0]
     binding.pry
     self.new_from_db(row)
   end
