0a50bf0d4646aca5452fd11131e48231fe896a70
Kriti Rai <kriti.rai@gmail.com>
2018-04-25 19:15:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,6 +61,7 @@ class Student
 
   def self.find_by_name(name)
     row = DB[:conn].execute("SELECT * FROM students WHERE name = ? LIMIT 1", name)[0]
+    binding.pry
     self.new_from_db(row)
   end
 end
